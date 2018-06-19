c9f90efe45bf5b3f02f151f4d6bf05909249cda6
HARRISKING <llc13588440410@outlook.com>
2018-06-19 15:21:37 +0800

    all test is over

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
-// var app = new Vue({
-//     el: "#app",
-//     data: {
-//         message: "hello harrisking"
-//     }
-// })
+var app = new Vue({
+    el: "#app",
+    data: {
+        message: "hello harrisking"
+    }
+})
 
-// var app2 = new Vue({
-//     el: '#app2',
-//     data:{
-//         message: '页面加在于 ' + new Date().toLocaleString()
-//     }
-// })
+var app2 = new Vue({
+    el: '#app2',
+    data:{
+        message: '页面加在于 ' + new Date().toLocaleString()
+    }
+})
 
-// var app3 = new Vue({
-//     el: '#app3',
-//     data: {
-//         seen:true
-//     }
-// })
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        seen:true
+    }
+})
 
-// var app4 = new Vue({
-//     el:'#app4',
-//     data:{
-//         todos: [
-//             {text: "饿问问"},
-//             {text: "通过天堂"},
-//             {text: "体育教育家"}
-//         ]
-//     }
-// })
+var app4 = new Vue({
+    el:'#app4',
+    data:{
+        todos: [
+            {text: "饿问问"},
+            {text: "通过天堂"},
+            {text: "体育教育家"}
+        ]
+    }
+})
 
-// var app5 = new Vue({
-//     el:'#app5',
-//     data: {
-//         message: 'harrisking is so handsome!'
-//     },
-//     methods: {
-//         reverseMessage: function (){
-//             this.message = this.message.split('').reverse().join('')
-//         }
-//     }
-// })
+var app5 = new Vue({
+    el:'#app5',
+    data: {
+        message: 'harrisking is so handsome!'
+    },
+    methods: {
+        reverseMessage: function (){
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
  
 var app6 = new Vue({
     el:'#app6',
     data: {
         message: 'gdfdsfasef'
+    }
+})
+
+
+Vue.component('todo-list',{
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+    el: '#app7',
+    data: {
+        groceryList: [
+            {id:0, text:'蔬菜'},
+            {id:1, text:'酸奶'},
+            {id:2, text:'鸡蛋'}
+        ]
     }
 })
