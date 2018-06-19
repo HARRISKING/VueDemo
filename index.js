c124a4bdffc3759a06b99d586d9810b38d6ff8c8
HARRISKING <llc13588440410@outlook.com>
2018-06-19 11:33:44 +0800

    some test

---
+++ index.js
@@ -0,0 +1,31 @@
+// var app = new Vue({
+//     el: "#app",
+//     data: {
+//         message: "hello harrisking"
+//     }
+// })
+
+// var app2 = new Vue({
+//     el: '#app2',
+//     data:{
+//         message: '页面加在于 ' + new Date().toLocaleString()
+//     }
+// })
+
+// var app3 = new Vue({
+//     el: '#app3',
+//     data: {
+//         seen:true
+//     }
+// })
+
+var app4 = new Vue({
+    el:'#app4',
+    data:{
+        todos: [
+            {text: "饿问问"},
+            {text: "通过天堂"},
+            {text: "体育教育家"}
+        ]
+    }
+})
