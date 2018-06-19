5690900e33f8426ab198f5e9c42841fbef2d1c29
HARRISKING <llc13588440410@outlook.com>
2018-06-19 14:03:08 +0800

    all test is over

--- index.js
+++ index.js
@@ -19,13 +19,32 @@
 //     }
 // })
 
-var app4 = new Vue({
-    el:'#app4',
-    data:{
-        todos: [
-            {text: "饿问问"},
-            {text: "通过天堂"},
-            {text: "体育教育家"}
-        ]
+// var app4 = new Vue({
+//     el:'#app4',
+//     data:{
+//         todos: [
+//             {text: "饿问问"},
+//             {text: "通过天堂"},
+//             {text: "体育教育家"}
+//         ]
+//     }
+// })
+
+// var app5 = new Vue({
+//     el:'#app5',
+//     data: {
+//         message: 'harrisking is so handsome!'
+//     },
+//     methods: {
+//         reverseMessage: function (){
+//             this.message = this.message.split('').reverse().join('')
+//         }
+//     }
+// })
+ 
+var app6 = new Vue({
+    el:'#app6',
+    data: {
+        message: 'gdfdsfasef'
     }
 })
