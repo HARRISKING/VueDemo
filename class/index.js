9d967d7ccfb618ab6b4a9003ed6e9263695c9818
HARRISKING <llc13588440410@outlook.com>
2018-06-21 11:27:47 +0800

    new add files

---
+++ class/index.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        is: [
+            {message:"hahah"},
+            {message:"tatat"},
+            {message:"tatasartasdt"},
+            {message:"tataasdfast"},
+            {message:"tatatdzxfzxcbv dffd"},
+            {message:"tatadsxzfgbdfsgt"},
+            {message:"tatasdfgh4er大风刮大风t"},
+            {message:"tat山东分公司地方at"},
+            {message:"tat数人声鼎沸公司∂∂at"},
+
+        ]
+    }
+})
